4f1ce27eb3efa762e92c9708caf081bb425a71e6
iszkey <kisozaki1026@gmail.com>
2023-02-22 23:43:23 +0900

    QueryClientの設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: false, //fetch失敗時にリトライしない
+            refetchOnWindowFocus: false, //ブラウザに戻ったときに再フェッチしない
+            suspense: true, // react18 のsuspense機能を使用する
+        },
+    },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />;
+        </QueryClientProvider>
+    );
 }
