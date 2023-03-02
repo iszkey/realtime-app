831a9be3d668f8fcfdb0ae10f35303385cbf303f
iszkey <kisozaki1026@gmail.com>
2023-03-02 21:54:58 +0900

    updateProfileMutationの追加

--- hooks/useMutateProfile.ts
+++ hooks/useMutateProfile.ts
@@ -1,6 +1,7 @@
 import { useQueryClient, useMutation } from 'react-query';
 import { supabase } from '@/utils/supabase';
 import { Profile } from '@/types';
+import { profile } from 'console';
 
 export const useMutateProfile = () => {
     const queryClient = useQueryClient();
@@ -21,5 +22,24 @@ export const useMutateProfile = () => {
             },
         }
     );
-    return {};
+    const updateProfileMutation = () =>
+        useMutation(
+            async (profile: Omit<Profile, 'updated_at' | 'created_at'>) => {
+                const { data, error } = await supabase
+                    .from('profiles')
+                    .update(profile)
+                    .eq('id', profile.id);
+                if (error) throw new Error(error.message);
+                return data;
+            },
+            {
+                onSuccess: (res) => {
+                    queryClient.setQueryData(['profile'], res[0]);
+                },
+                onError: (err: any) => {
+                    alert(err.message);
+                },
+            }
+        );
+    return { createProfileMutation, updateProfileMutation };
 };
